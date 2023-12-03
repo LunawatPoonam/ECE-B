6d0d382a548932111f3722cb9c3017cb5b3afdb2
varshithasrisai <148360601+varshithasrisai@users.noreply.github.com>
2023-12-03 16:10:47 +0530

    Update

---
+++ a11.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+
+void insertion_str(char str1[50], char str2[50], char str3[100]);
+
+int main()
+{
+    char str1[50], str2[50], str3[100];
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str1);
+    getchar();
+
+    printf("Enter another string: ");
+    scanf("%[^\n]", str2);
+
+    insertion_str(str1, str2, str3);
+
+    return 0;
+}
+
+void insertion_str(char str1[50], char str2[50], char str3[100])
+{
+    int pos;
+
+    printf("Enter position : ");
+    scanf("%d", &pos);
+
+    strncpy(str3, str1, pos);
+    strcat(str3, str2);
+    strcat(str3, str1 + pos + 1);
+
+    printf("%s", str3);
+}
